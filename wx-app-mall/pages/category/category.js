fc86b8d7bbe24247b7e702a38a6c6bdeab0757e2
janice <1093492176@qq.com>
2019-09-27 16:51:42 +0800

    添加分类完成部分功能

--- wx-app-mall/pages/category/category.js
+++ wx-app-mall/pages/category/category.js
@@ -1,18 +1,26 @@
 // pages/category/category.js
+
+const WXAPI=require('../../wxapi/main.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    categoryToView: '',  // 左侧分类，设置哪个方向可滚动，则在哪个方向滚动到该元素
+    goodsToView:'',
+    categories:[],
+    goodsList:[],
+    currentCategoryId:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showNavigationBarLoading()
+    this.getGoodsCategory()
   },
 
   /**
@@ -62,5 +70,93 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 获取商品分类
+  getGoodsCategory:function(){
+    const _this=this;
+    WXAPI.getGoodsCategory().then((res)=>{
+      const data=res && res.data;
+      _this.converCategories(data,_this)
+    }).catch((e)=>{
+      wx.hideNavigationBarLoading()
+    })
+  },
+
+  // 对商品分类进行数据转换
+  converCategories:function(data,_this){
+    const categories=[]
+    data.forEach((value,index)=>{
+      value.scrollId="scroll"+value.id
+      categories.push(value)
+      if(index===0){
+        _this.setData({
+          currentCategoryId:value.scrollId
+        })
+      }
+    })
+    _this.setData({
+      categories:categories
+    })
+    this.getGoodsList()
+  },
+
+  // 点击分类显示商品信息
+  handleCategoryClick:function(e){
+    console.log(e)
+    const id = e.currentTarget.id
+    this.setData({
+      currentCategoryId: id,
+      goodsToView:id
+    })
+  },
+
+  // 获取商品信息
+  getGoodsList:function(){
+    const _this=this;
+    WXAPI.getGoodsList({
+      categoryId:'',
+      page:1,
+      pageSize:100000
+    }).then((res)=>{
+      console.log(res)
+      const data=res && res.data
+      const goodsList=[]
+      _this.data.categories.forEach((value,index)=>{
+        let temp={}
+        let goods=[]
+        temp.id=value.id
+        temp.scrollId='scroll'+value.id
+        temp.name=value.name
+        temp.goods=goods
+        console.log(temp)
+
+        data.forEach((item)=>{
+          if(item.categoryId==temp.id){
+            goods.push(item)
+          }
+        })
+
+        goodsList.push(temp)
+      })
+      console.log(goodsList)
+      _this.setData({
+        goodsList:goodsList
+      })
+      wx.hideNavigationBarLoading()
+    }).catch((e)=>{
+      wx.hideNavigationBarLoading()
+    })
+  },
+
+  // 点击进入商品详情
+  handleGoodDetail:function(e){
+
+  },
+
+  // 滚动时触发
+  handleScroll:function(e){
+
   }
+
 })
